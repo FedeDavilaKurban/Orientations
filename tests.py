8cad10a4f75ab4afdbb28bb2e2a00276521c413a
FedeDavilaKurban <fdavilakurban@unc.edu.ar>
2020-12-08 21:15:04 -0300

    minor change

--- tests.py
+++ tests.py
@@ -9,11 +9,9 @@ zeros = np.zeros((1,N))
 
 cos,y,ecdf,yfit,d_yfit,a2 = fits(ones.tolist())
 print(a2)
-#a2string="{:.3f}".format(a2)
 ascii.write(Table(np.column_stack([cos,ecdf(cos),y,yfit,d_yfit])),'../data/fits_ones'.format(sec,rmin,rmax,s5),names=['cos','ecdf','y','yfit','d_yfit'],overwrite=True)
 
 
 cos,y,ecdf,yfit,d_yfit,a2 = fits(zeros.tolist())
 print(a2)
-#a2string="{:.3f}".format(a2)
 ascii.write(Table(np.column_stack([cos,ecdf(cos),y,yfit,d_yfit])),'../data/fits_zeros'.format(sec,rmin,rmax,s5),names=['cos','ecdf','y','yfit','d_yfit'],overwrite=True)
