612fad4f4c344d8af03e880038d36e0dfeafe864
FedeDavilaKurban <fdavilakurban@unc.edu.ar>
2020-12-09 12:26:11 -0300

    minor changes

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import numpy as np
 from orientationsTools import *
 from astropy.io import ascii
+from astropy.table import Table
 
 N=1000
 
@@ -9,9 +10,9 @@ zeros = np.zeros((1,N))
 
 cos,y,ecdf,yfit,d_yfit,a2 = fits(ones.tolist())
 print(a2)
-ascii.write(Table(np.column_stack([cos,ecdf(cos),y,yfit,d_yfit])),'../data/fits_ones'.format(sec,rmin,rmax,s5),names=['cos','ecdf','y','yfit','d_yfit'],overwrite=True)
+ascii.write(Table(np.column_stack([cos,ecdf(cos),y,yfit,d_yfit])),'../data/fits_ones',names=['cos','ecdf','y','yfit','d_yfit'],overwrite=True)
 
 
 cos,y,ecdf,yfit,d_yfit,a2 = fits(zeros.tolist())
 print(a2)
-ascii.write(Table(np.column_stack([cos,ecdf(cos),y,yfit,d_yfit])),'../data/fits_zeros'.format(sec,rmin,rmax,s5),names=['cos','ecdf','y','yfit','d_yfit'],overwrite=True)
+ascii.write(Table(np.column_stack([cos,ecdf(cos),y,yfit,d_yfit])),'../data/fits_zeros',names=['cos','ecdf','y','yfit','d_yfit'],overwrite=True)
